73e0b3a9043e200b751fa6de66535fd75dee10d3
Marcela Chuqi <81633985+marcelachuqi@users.noreply.github.com>
2021-06-16 20:49:06 +0200

    add changes to route

--- src/components/CartoonDetail.js
+++ src/components/CartoonDetail.js
@@ -1,14 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const CartoonDetail = (props) => {
   return (
-    <>
+    <div className="cartoon-detail">
+      <Link to="/">Volver</Link>
       <li>
         <img src={props.cartoon.image} alt={props.cartoon.name} />
         <h1> {props.cartoon.name} </h1>
         <p> {props.cartoon.species}</p>
       </li>
-    </>
+    </div>
   );
 };
 
